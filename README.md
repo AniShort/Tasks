70f082303f631bf76b377e7acc53db6dfe11a8c4
AniShort <shortanijith@gmail.com>
2024-09-23 19:21:32 -0400

    Added TODOs and README

---
+++ README.md
@@ -0,0 +1,2 @@
+# TASKS
+This is a simple clean tasks app that stores all you tasks locally, removing any security needs. Basically Obsidian for tasks, hopefully
--- obj/Debug/net8.0/Tasks.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tasks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91d4a2b2365caecf593bf8568197367933c4b0d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
